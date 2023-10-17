579f4bcd70a925a9f562d0e53e066de8e360f3f7
seshanb <125367985+seshanb@users.noreply.github.com>
2023-10-17 15:35:17 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,10 +1,31 @@
-provider_init {
-  enabled = false
+variable "resource_group_name" {
+  description = "The name of the Azure Resource Group."
+  type        = string
+  default     = "tf-test"
 }
-resource "null_resource" "sleep" {
 
-  # Execute the sleep command when the resource is created.
-  provisioner "local-exec" {
-    command = "sleep 600"
-  }
+variable "location" {
+  description = "The location for Azure resources."
+  type        = string
+  default     = "West Europe"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_application_insights" "example" {
+  name                = "tf-test-appinsights"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  application_type    = "web"
+}
+
+output "instrumentation_key" {
+  value = azurerm_application_insights.example.instrumentation_key
+}
+
+output "app_id" {
+  value = azurerm_application_insights.example.app_id
 }
