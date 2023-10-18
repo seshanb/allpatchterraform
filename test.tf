d1baa9a81849ba460774a5cae35e57138324ca90
seshanb <125367985+seshanb@users.noreply.github.com>
2023-10-18 16:16:27 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -7,27 +7,10 @@ variable "resource_group_name" {
 variable "location" {
   description = "The location for Azure resources."
   type        = string
-  default     = "West Europe"
+  default     = "eastus2"
 }
 
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
-}
-
-resource "azurerm_application_insights" "example" {
-  name                = "tf-test-appinsights"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  application_type    = "web"
-}
-
-output "instrumentation_key" {
-  value = azurerm_application_insights.example.instrumentation_key
-  sensitive = true
-}
-
-output "app_id" {
-  value = azurerm_application_insights.example.app_id
-  sensitive = true
 }
