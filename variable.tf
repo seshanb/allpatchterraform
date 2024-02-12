60d9d6bde73b5f4ce10a6a9f4e60bb07739bf1d3
seshanb <125367985+seshanb@users.noreply.github.com>
2024-02-12 16:38:33 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,5 +1,22 @@
-variable "resource_group_name" {
-  description = "The name of the Azure Resource Group."
-  type        = string
-  default     = "tftest"
+variable "aws_region" {
+  description = "The AWS region where the S3 bucket will be created."
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket."
+}
+
+variable "acl" {
+  description = "The access control for the S3 bucket."
+  default     = "private"
+}
+
+variable "tags" {
+  description = "Tags to apply to the S3 bucket."
+  type        = map(string)
+  default     = {
+    Name        = "MyBucket"
+    Environment = "Production"
+  }
 }
